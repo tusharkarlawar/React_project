a907e6abecf6a2db341a9be6b7c64a9c24819b93
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-06-30 07:33:21 +0530

    ExpenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <h2>Expense item</h2>
+            <div>
+               <h3>Food Rs 10</h3>
+               <h3>Petrol Rs 100</h3>
+               <h3>Movies Rs 200</h3>
+            </div>
+            
+        </div>
+
+    );
+}
+
+export default ExpenseItem;
