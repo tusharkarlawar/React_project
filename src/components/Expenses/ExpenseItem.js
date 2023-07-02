384881cd2c9f5d7fc7111dad1760003044db1738
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-07-02 15:57:20 +0530

    PlaceHolder - Adding a filter which doesnt work on the expense tracker

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -6,31 +6,16 @@ import './ExpenseItem.css';
 
 
 const ExpenseItem = (props) => {
-  //  fuction clickHandler() {} //
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React')
-
-  const clickHandler = () => {
-    setTitle('Update!');
-
-    console.log(title);
-   };
-   
-      
-
-
-    
-    
+  
     return (
         <Card className="expense-item">
           <ExpenseDate date={props.date} />
           
-          <div className="expense-item__description">
-               <h2>{title}</h2>
+           <div className="expense-item__description">
+               <h2>{props.title}</h2>
                <div className="expense-item__price">{props.location}</div>
                <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
             
         </Card>
 
